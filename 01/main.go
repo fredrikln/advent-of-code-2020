2842b8738a2c87c96f2143d24f2e85b07dcce1fc
Fredrik Larsson <fredrik.larsson@enklare.se>
2020-12-01 06:48:52 +0100

    Finished Day 01

--- 01/main.go
+++ 01/main.go
@@ -1,15 +1,46 @@
 package main
 
-func part1(name string) string {
+import (
+	"fmt"
 
+	"github.com/fredrikln/advent-of-code-2020/utils"
+)
+
+func part1(expenses []int) int {
+	fmt.Println("Part 1")
+	for _, i := range expenses {
+		for _, j := range expenses {
+			if (i + j) == 2020 {
+				return i * j
+			}
+		}
+	}
+
+	return 0
 }
 
-func part2() {
+func part2(expenses []int) int {
+	fmt.Println("Part 2")
 
+	for _, i := range expenses {
+		for _, j := range expenses {
+			for _, k := range expenses {
+				if (i + j + k) == 2020 {
+					return i * j * k
+				}
+			}
+		}
+	}
+
+	return 0
 }
 
 func main() {
-	part1()
+	input := utils.ReadInputLinesAsNumbers()
 
-	part2()
+	result1 := part1(input)
+	fmt.Println(result1)
+
+	result2 := part2(input)
+	fmt.Println(result2)
 }
--- utils/utils.go
+++ utils/utils.go
@@ -2,19 +2,66 @@ package utils
 
 import (
 	"bufio"
+	"fmt"
+	"io/ioutil"
 	"os"
+	"strconv"
+	"strings"
 )
 
-// ReadInput reads input file lines into an array
+// ReadInput reads file's lines into array of strings
 func ReadInput() []string {
-
 	var input []string
 
 	f, _ := os.Open("input.txt")
+	defer f.Close()
 	scanner := bufio.NewScanner(f)
 
 	for scanner.Scan() {
 		input = append(input, scanner.Text())
 	}
+
+	return input
+}
+
+// ReadInputLinesAsNumbers reads file's lines into an array of numbers
+func ReadInputLinesAsNumbers() []int {
+	var input []int
+
+	f, _ := os.Open("input.txt")
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		var n int
+		_, err := fmt.Sscanf(scanner.Text(), "%d", &n)
+		if err != nil {
+			panic(err)
+		}
+
+		input = append(input, n)
+	}
+
+	return input
+}
+
+// ReadInputAsNumbers reads file as line of numbers
+func ReadInputAsNumbers() []int {
+	var input []int
+
+	content, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	text := strings.Trim(string(content), "\r\n")
+
+	tokens := strings.Split(text, ", ")
+
+	for _, i := range tokens {
+		val, _ := strconv.Atoi(i)
+		input = append(input, val)
+	}
+
 	return input
 }
