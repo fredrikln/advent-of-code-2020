fc41ced362355e2c34fe8f9432e0469c02a53d78
Fredrik Larsson <fredrik.larsson@enklare.se>
2020-12-07 07:42:09 +0100

    Day 7 completed

--- 07/main.go
+++ 07/main.go
@@ -1,13 +1,108 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/fredrikln/advent-of-code-2020/utils"
+)
+
 func main() {
+	input := utils.ReadInput("input.txt")
 
+	bags := parseBags(input)
+
+	fmt.Println("Part 1")
+	result := part1(bags, "shiny gold")
+	fmt.Println("Count:", result)
+
+	fmt.Println("Part 2")
+	result2 := part2(bags, "shiny gold")
+	fmt.Println("Count:", result2)
 }
 
-func part1() {
-
+type content struct {
+	count int
+	bag   *bag
+}
+type bag struct {
+	contains map[string]*content
 }
 
-func part2() {
+func parseBags(input []string) map[string]*bag {
+	bags := make(map[string]*bag)
 
+	for _, line := range input {
+		firstBags := strings.Index(line, " bags")
+
+		color := line[:firstBags]
+		re := regexp.MustCompile("([0-9]+) ([a-z ]+) bag")
+
+		contains := re.FindAllStringSubmatch(line, -1)
+
+		curBag, ok := bags[color]
+		if !ok {
+			bags[color] = &bag{make(map[string]*content, 0)}
+			curBag = bags[color]
+		}
+
+		for _, bagmatch := range contains {
+			theBag, ok := bags[bagmatch[2]]
+			if !ok {
+				bags[bagmatch[2]] = &bag{make(map[string]*content, 0)}
+				theBag = bags[bagmatch[2]]
+			}
+
+			num, err := strconv.Atoi(bagmatch[1])
+			if err != nil {
+				panic(err)
+			}
+
+			curBag.contains[bagmatch[2]] = &content{num, theBag}
+		}
+	}
+
+	return bags
+}
+
+func contains(curBag *bag, color string) bool {
+	_, ok := curBag.contains[color]
+	if ok {
+		return true
+	}
+
+	for _, content := range curBag.contains {
+		if contains(content.bag, color) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func part1(bags map[string]*bag, color string) int {
+	var count int
+	for _, bag := range bags {
+		if contains(bag, color) {
+			count++
+		}
+	}
+	return count
+}
+
+func countSubBags(bag *bag) int {
+	count := 0
+	for _, contains := range bag.contains {
+		count += contains.count + contains.count*countSubBags(contains.bag)
+	}
+
+	return count
+}
+
+func part2(bags map[string]*bag, color string) int {
+	bag := bags[color]
+
+	return countSubBags(bag)
 }
--- 07/main_test.go
+++ 07/main_test.go
@@ -1,1 +1,123 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_parseBags(t *testing.T) {
+	type args struct {
+		input []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"Example 1",
+			args{[]string{
+				"light red bags contain 1 bright white bag, 2 muted yellow bags.",
+				"dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+				"bright white bags contain 1 shiny gold bag.",
+				"muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+				"shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+				"dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+				"vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+				"faded blue bags contain no other bags.",
+				"dotted black bags contain no other bags.",
+			}},
+			9,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := parseBags(tt.args.input); !reflect.DeepEqual(len(got), tt.want) {
+				t.Errorf("parseBags() = %v, want %v", len(got), tt.want)
+			}
+		})
+	}
+}
+
+func Test_part1(t *testing.T) {
+	type args struct {
+		input []string
+		color string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"Example 1",
+			args{[]string{
+				"light red bags contain 1 bright white bag, 2 muted yellow bags.",
+				"dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+				"bright white bags contain 1 shiny gold bag.",
+				"muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+				"shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+				"dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+				"vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+				"faded blue bags contain no other bags.",
+				"dotted black bags contain no other bags.",
+			}, "shiny gold"},
+			4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part1(parseBags(tt.args.input), tt.args.color); got != tt.want {
+				t.Errorf("part1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_part2(t *testing.T) {
+	type args struct {
+		input []string
+		color string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"Example 1",
+			args{[]string{
+				"light red bags contain 1 bright white bag, 2 muted yellow bags.",
+				"dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+				"bright white bags contain 1 shiny gold bag.",
+				"muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+				"shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+				"dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+				"vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+				"faded blue bags contain no other bags.",
+				"dotted black bags contain no other bags.",
+			}, "shiny gold"},
+			32,
+		},
+		{
+			"Example 1",
+			args{[]string{
+				"shiny gold bags contain 2 dark red bags.",
+				"dark red bags contain 2 dark orange bags.",
+				"dark orange bags contain 2 dark yellow bags.",
+				"dark yellow bags contain 2 dark green bags.",
+				"dark green bags contain 2 dark blue bags.",
+				"dark blue bags contain 2 dark violet bags.",
+				"dark violet bags contain no other bags.",
+			}, "shiny gold"},
+			126,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part2(parseBags(tt.args.input), tt.args.color); got != tt.want {
+				t.Errorf("part2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
