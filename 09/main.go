da097ef1f5ac10bc61f763843e0d53318426c475
Fredrik Larsson <fredrik.larsson@enklare.se>
2020-12-09 07:59:44 +0100

    Day 9 completed

--- 09/main.go
+++ 09/main.go
@@ -1,13 +1,98 @@
 package main
 
+import (
+	"fmt"
+	"math"
+
+	"github.com/fredrikln/advent-of-code-2020/utils"
+)
+
 func main() {
+	input := utils.ReadInputLinesAsNumbers()
 
+	fmt.Println("Part 1")
+	result := part1(input)
+	fmt.Println("First invalid:", result)
+
+	fmt.Println("Part 2")
+	result2 := part2(input, result)
+	fmt.Println("Sum:", result2)
 }
 
-func part1() {
+func firstInvalid(numbers []int, preamble int) int {
+	numbersSoFar := make([]int, preamble, len(numbers))
+	copy(numbersSoFar, numbers[:preamble])
 
+	for _, n := range numbers[preamble:] {
+		if !isValidNumber(numbersSoFar, n, preamble) {
+			return n
+		}
+
+		numbersSoFar = append(numbersSoFar, n)
+	}
+
+	return 0
 }
 
-func part2() {
+func isValidNumber(previous []int, number int, preamble int) bool {
+	numbersToConsider := previous[len(previous)-preamble:]
 
+	for i := 0; i < len(numbersToConsider); i++ {
+		for j := i + 1; j < len(numbersToConsider); j++ {
+			if numbersToConsider[i]+numbersToConsider[j] == number {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func sum(numbers []int) int {
+	var sum int
+
+	for _, i := range numbers {
+		sum += i
+	}
+
+	return sum
+}
+
+func findContiguous(numbers []int, number int) []int {
+	for i := 2; i < number/2; i++ {
+		for j := 0; j < len(numbers)-i; j++ {
+			if sum(numbers[j:j+i]) == number {
+				return numbers[j : j+i]
+			}
+		}
+	}
+
+	return make([]int, 0)
+}
+
+func minMax(numbers []int) (int, int) {
+	min, max := math.MaxInt64, math.MinInt64
+
+	for _, i := range numbers {
+		if i < min {
+			min = i
+		}
+		if i > max {
+			max = i
+		}
+	}
+
+	return min, max
+}
+
+func part1(numbers []int) int {
+	return firstInvalid(numbers, 25)
+}
+
+func part2(numbers []int, number int) int {
+	contiguous := findContiguous(numbers, number)
+
+	min, max := minMax(contiguous)
+
+	return min + max
 }
--- 09/main_test.go
+++ 09/main_test.go
@@ -1,1 +1,187 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_isValidNumber(t *testing.T) {
+	type args struct {
+		previous []int
+		number   int
+		preamble int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"Example 1",
+			args{
+				[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25},
+				26,
+				25,
+			},
+			true,
+		},
+		{
+			"Example 2",
+			args{
+				[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25},
+				49,
+				25,
+			},
+			true,
+		},
+		{
+			"Example 3",
+			args{
+				[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25},
+				100,
+				25,
+			},
+			false,
+		},
+		{
+			"Example 4",
+			args{
+				[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25},
+				50,
+				25,
+			},
+			false,
+		},
+		{
+			"Example 5",
+			args{
+				[]int{20, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25, 45},
+				26,
+				25,
+			},
+			true,
+		},
+		{
+			"Example 6",
+			args{
+				[]int{20, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25, 45},
+				65,
+				25,
+			},
+			false,
+		},
+		{
+			"Example 7",
+			args{
+				[]int{20, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25, 45},
+				64,
+				25,
+			},
+			true,
+		},
+		{
+			"Example 8",
+			args{
+				[]int{20, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25, 45},
+				66,
+				25,
+			},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isValidNumber(tt.args.previous, tt.args.number, tt.args.preamble); got != tt.want {
+				t.Errorf("isValidNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_firstInvalid(t *testing.T) {
+	type args struct {
+		numbers  []int
+		preamble int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Example 1", args{[]int{35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576}, 5}, 127},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := firstInvalid(tt.args.numbers, tt.args.preamble); got != tt.want {
+				t.Errorf("firstInvalid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_findContiguous(t *testing.T) {
+	type args struct {
+		numbers []int
+		number  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"Example 1", args{[]int{35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576}, 127}, []int{15, 25, 47, 40}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findContiguous(tt.args.numbers, tt.args.number); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findContiguous() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_minMax(t *testing.T) {
+	type args struct {
+		numbers []int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  int
+		want1 int
+	}{
+		{"Example 1", args{[]int{1, 2, 3, 4, 5}}, 1, 5},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := minMax(tt.args.numbers)
+			if got != tt.want {
+				t.Errorf("minMax() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("minMax() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func Test_part2(t *testing.T) {
+	type args struct {
+		numbers []int
+		number  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Example 1", args{[]int{35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576}, 127}, 62},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part2(tt.args.numbers, tt.args.number); got != tt.want {
+				t.Errorf("part2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
